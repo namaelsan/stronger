1cae99a54552d549f5e8e5d145311bb99f007cbe
namaelsan <x.arybidx@gmail.com>
2024-12-06 16:36:21 +0300

    stats and programs init

---
+++ lib/programs.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProgramsPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _ProgramsPageState();
+  }
+}
+
+class _ProgramsPageState extends State<ProgramsPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+
+    );
+  }
+
+}
---
+++ lib/stats.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StatsPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _StatsPageState();
+  }
+}
+
+class _StatsPageState extends State<StatsPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+
+    );
+  }
+
+}
