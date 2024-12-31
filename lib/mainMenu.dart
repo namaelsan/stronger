0a9407b37f6117ac929808d7321a2347f96dd023
namaelsan <x.arybidx@gmail.com>
2024-12-31 18:24:48 +0300

    added settings to bottombar

--- lib/mainMenu.dart
+++ lib/mainMenu.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'mylib.dart';
 import 'stats.dart';
 import 'programs.dart';
+import 'settings.dart';
 
 class MainMenu extends StatefulWidget{
   @override
@@ -15,14 +16,15 @@ class _MainMenuState extends State<MainMenu>{
   int _currentIndex = 0;
 
   final List<Widget> _pages = [
-    StatsPage(),
     ProgramsPage(),
+    StatsPage(),
+    SettingsPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Stronger",style: Theme.of(context).textTheme.headlineMedium,)),
+      // appBar: AppBar(title: Text("Stronger",style: Theme.of(context).textTheme.headlineMedium,)),
       body: IndexedStack(
         index: _currentIndex,
         children: _pages,
@@ -35,9 +37,9 @@ class _MainMenuState extends State<MainMenu>{
             });
           },
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Option1"),
-            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Option2"),
-            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Option3"),
+            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Programs"),
+            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Graphs"),
+            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Settings"),
 
           ],
       ),
