860b5995abbcf6772bb6a8bc565de1747ba3a866
namaelsan <x.arybidx@gmail.com>
2025-01-02 16:43:47 +0300

    added workouts

--- lib/mainMenu.dart
+++ lib/mainMenu.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
-import 'mylib.dart';
 import 'stats.dart';
 import 'Programs/programs.dart';
 import 'settings.dart';
+import 'workouts.dart';
 
-class MainMenu extends StatefulWidget{
+class MainMenu extends StatefulWidget {
+  const MainMenu({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return _MainMenuState();
   }
 }
 
-class _MainMenuState extends State<MainMenu>{
-
+class _MainMenuState extends State<MainMenu> {
   int _currentIndex = 0;
 
   final List<Widget> _pages = [
     ProgramsPage(),
+    WorkoutsPage(),
     StatsPage(),
     SettingsPage(),
   ];
@@ -24,25 +26,25 @@ class _MainMenuState extends State<MainMenu>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: Text("Stronger",style: Theme.of(context).textTheme.headlineMedium,)),
       body: IndexedStack(
         index: _currentIndex,
         children: _pages,
       ),
       bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index){
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Programs"),
-            BottomNavigationBarItem(icon: Icon(Icons.bar_chart),label: "Graphs"),
-            BottomNavigationBarItem(icon: Icon(Icons.settings),label: "Settings"),
-          ],
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Programs"),
+          BottomNavigationBarItem(icon: Icon(Icons.access_time), label: "Workouts"),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: "Graphs"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
+        ],
       ),
     );
   }
-
 }
