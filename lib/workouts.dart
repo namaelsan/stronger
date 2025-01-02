beba7c7d534f753e5554f8c999d8ee1bd954cad2
namaelsan <x.arybidx@gmail.com>
2025-01-02 19:16:40 +0300

    bg

--- lib/workouts.dart
+++ lib/workouts.dart
@@ -10,55 +10,57 @@ class WorkoutsPage extends StatelessWidget {
     var screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            SizedBox(height: screenSize.height * 0.035),
-            // Page Title
-            Center(
-              child: Stack(
-                children: [
-                  Text(
-                    "Past Workouts",
-                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      foreground: Paint()
-                        ..style = PaintingStyle.stroke
-                        ..strokeWidth = 7
-                        ..color = AppTheme.colorDark1,
-                    ),
-                  ),
-                  Text(
-                    "Past Workouts",
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20), // Add some spacing
-            // Use Expanded to allow ListView to take up remaining space
-            Expanded(
-              child: ListView.builder(
-                itemCount: workoutProvider.pastWorkouts.length,
-                itemBuilder: (context, index) {
-                  final workout = workoutProvider.pastWorkouts[index];
-                  return Card(
-                    margin: EdgeInsets.symmetric(vertical: 8.0),
-                    child: ListTile(
-                      title: Text(workout.title),
-                      subtitle: Text(
-                        "${workout.getExerciseAmount()} Movements · ${workout.getSetAmount()} Sets",
+      body: ReusableWidgets.myBackgroundGradient(
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                SizedBox(height: screenSize.height * 0.035),
+                // Page Title
+                Center(
+                  child: Stack(
+                    children: [
+                      Text(
+                        "Past Workouts",
+                        style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 7
+                            ..color = AppTheme.colorDark1,
+                        ),
                       ),
-                      onTap: () {
-                        // Navigate to a detailed view of the workout if needed
-                      },
-                    ),
-                  );
-                },
-              ),
+                      Text(
+                        "Past Workouts",
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20), // Add some spacing
+                // Use Expanded to allow ListView to take up remaining space
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: workoutProvider.pastWorkouts.length,
+                    itemBuilder: (context, index) {
+                      final workout = workoutProvider.pastWorkouts[index];
+                      return Card(
+                        margin: EdgeInsets.symmetric(vertical: 8.0),
+                        child: ListTile(
+                          title: Text(workout.title),
+                          subtitle: Text(
+                            "${workout.getExerciseAmount()} Movements · ${workout.getSetAmount()} Sets · ${workout.time} Seconds",
+                          ),
+                          onTap: () {
+                            // Navigate to a detailed view of the workout if needed
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
       ),
     );
   }
