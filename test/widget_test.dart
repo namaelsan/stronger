da34739bc9997f29009897fabdb6db10303c5ce7
namaelsan <x.arybidx@gmail.com>
2025-01-03 21:12:09 +0300

    rename init.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stronger/init.dart';
+import 'package:stronger/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
