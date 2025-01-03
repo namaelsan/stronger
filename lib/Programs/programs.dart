2a3429b4901b8c3875091b70dba82fa41df3ff6b
namaelsan <x.arybidx@gmail.com>
2025-01-03 04:13:00 +0300

    scrollable + background

--- lib/Programs/programs.dart
+++ lib/Programs/programs.dart
@@ -13,6 +13,226 @@ class ProgramsPage extends StatefulWidget {
 }
 
 class _ProgramsPageState extends State<ProgramsPage> {
+  void navigateToCreateProgram(List<Program> programs, int index,
+      {required bool isNew}) async {
+    var updatedProgram = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CreateProgramsPage(
+          programs: programs,
+          index: index,
+          isNew: isNew,
+        ),
+      ),
+    );
+
+    if (updatedProgram == null) {
+      return;
+    }
+    setState(() {
+      if (isNew) {
+        userPrograms.add(updatedProgram.copy());
+      } else {
+        userPrograms[index] = updatedProgram; // Update the program list
+      }
+    });
+  }
+
+  void navigateToStartWorkout(
+    List<Program> programs,
+    int index,
+  ) async {
+    var updatedProgram = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => StartWorkout(
+          program: programs[index],
+        ),
+      ),
+    );
+
+    if (updatedProgram == null) {
+      return;
+    }
+    setState(() {
+      programs[index] = updatedProgram.copy(); // Update the program list
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          navigateToCreateProgram(userPrograms, -1, isNew: true);
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      body: ReusableWidgets.myBackgroundGradient(
+        ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            SizedBox(height: screenSize.height * 0.035),
+            // Page Title
+            Center(
+              child: Stack(
+                children: [
+                  Text(
+                    "Your Programs",
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 7
+                            ..color = AppTheme.colorDark1,
+                        ),
+                  ),
+                  Text(
+                    "Your Programs",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            // Template Programs Section
+            Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Container(
+                // width: screenSize.width * 0.9, // Constrain the width
+                child: Card(
+                  elevation: 4,
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(7, 2, 7, 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Templates",
+                          style: AppTheme.myTheme.textTheme.headlineSmall
+                              ?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'YourCustomFont',
+                            color: AppTheme.colorDark1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+            SizedBox(height: 10),
+            SizedBox(
+              height: 150,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: templatePrograms.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      navigateToCreateProgram(templatePrograms, index,
+                          isNew: true);
+                    },
+                    child: Card(
+                      margin: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Container(
+                        width: screenSize.width * 0.38,
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              templatePrograms[index].title,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium
+                                  ?.copyWith(color: Colors.black),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              templatePrograms[index].description ??
+                                  "Description here",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium
+                                  ?.copyWith(
+                                      color: Colors.black.withOpacity(0.6)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            // User Programs Section
+            Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Container(
+                // width: screenSize.width * 0.9, // Constrain the width
+                child: Card(
+                  elevation: 4,
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(7, 2, 7, 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Your Programs",
+                          style: AppTheme.myTheme.textTheme.headlineSmall
+                              ?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'YourCustomFont',
+                            color: AppTheme.colorDark1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+            // User Programs List
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: userPrograms.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8.0),
+                  child: ListTile(
+                    title: Text(userPrograms[index].title),
+                    subtitle: Text(
+                      "${userPrograms[index].getExerciseAmount()} Movements · ${userPrograms[index].getSetAmount()} Sets",
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.edit, color: AppTheme.colorDark1),
+                      tooltip: 'Edit Program',
+                      onPressed: () {
+                        navigateToCreateProgram(userPrograms, index,
+                            isNew: false);
+                      },
+                    ),
+                    onTap: () {
+                      navigateToStartWorkout(userPrograms, index);
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   List<Program> userPrograms = [
     Program("Beginner program",
@@ -88,221 +308,6 @@ class _ProgramsPageState extends State<ProgramsPage> {
           Exercise("Squat", sets: [Set(15, "Warmup")])
         ])
   ];
-
-  void navigateToCreateProgram(List<Program> programs, int index,
-      {required bool isNew}) async {
-    var updatedProgram = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CreateProgramsPage(
-          programs: programs,
-          index: index,
-          isNew: isNew,
-        ),
-      ),
-    );
-
-    if (updatedProgram == null) {
-      return;
-    }
-    setState(() {
-      if (isNew) {
-        userPrograms.add(updatedProgram.copy());
-      } else {
-        userPrograms[index] = updatedProgram; // Update the program list
-      }
-    });
-  }
-
-  void navigateToStartWorkout(List<Program> programs, int index,) async {
-    var updatedProgram = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => StartWorkout(
-          program: programs[index],
-        ),
-      ),
-    );
-
-    if (updatedProgram == null) {
-      print("123");
-      return;
-    }
-    print("1235");
-    setState(() {
-        programs[index] = updatedProgram.copy(); // Update the program list
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
-
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          navigateToCreateProgram(userPrograms, -1, isNew: true);
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: screenSize.height * 0.035),
-
-              // Page Title
-              Center(
-                child: Stack(
-                  children: [
-                    Text(
-                      "Your Programs",
-                      style:
-                          Theme.of(context).textTheme.headlineLarge?.copyWith(
-                                foreground: Paint()
-                                  ..style = PaintingStyle.stroke
-                                  ..strokeWidth = 7
-                                  ..color = AppTheme.colorDark1,
-                              ),
-                    ),
-                    Text(
-                      "Your Programs",
-                      style: Theme.of(context).textTheme.headlineLarge,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Template Programs Section
-              Card(
-                elevation: 4,
-                margin: EdgeInsets.symmetric(vertical: 8),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(7, 2, 7, 2),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                          "Templates",
-                          style: AppTheme.myTheme.textTheme.headlineSmall?.copyWith(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'YourCustomFont',
-                            color: AppTheme.colorDark1
-                          )
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              SizedBox(
-                height: 150,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: templatePrograms.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        navigateToCreateProgram(templatePrograms, index,
-                            isNew: true);
-                      },
-                      child: Card(
-                        margin: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Container(
-                          width: screenSize.width * 0.38,
-                          padding: EdgeInsets.all(16),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                templatePrograms[index].title,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .titleMedium
-                                    ?.copyWith(color: Colors.black),
-                              ),
-                              SizedBox(height: 10),
-                              Text(
-                                templatePrograms[index].description ??
-                                    "Description here",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium
-                                    ?.copyWith(
-                                        color: Colors.black.withOpacity(0.6)),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // User Programs Section
-              Card(
-                elevation: 4,
-                margin: EdgeInsets.symmetric(vertical: 8),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(7, 2, 7, 2),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                          "Your Programs",
-                          style: AppTheme.myTheme.textTheme.headlineSmall?.copyWith(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'YourCustomFont',
-                            color: AppTheme.colorDark1
-                          )
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              // TODO: CHANGE THIS BUILDER FROM TEMPLATE TO ACTUAL PROGRAMS
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: userPrograms.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: EdgeInsets.symmetric(vertical: 8.0),
-                    child: ListTile(
-                      title: Text(userPrograms[index].title),
-                      subtitle: Text(
-                        "${userPrograms[index].getExerciseAmount()} Movements · ${userPrograms[index].getSetAmount()} Sets",
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.edit, color: AppTheme.colorDark1),
-                        tooltip: 'Edit Program',
-                        onPressed: () {
-                          navigateToCreateProgram(userPrograms, index,
-                              isNew: false);
-                        },
-                      ),
-                      onTap: () {
-                        navigateToStartWorkout(userPrograms,index);
-                      },
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
 
 // Class representing a Set
@@ -377,9 +382,9 @@ class Program {
   List<Exercise>? exercises;
   String? description;
   String title;
-  int time = -1;
+  int? time;
 
-  Program(this.title, {this.exercises, this.description});
+  Program(this.title, {this.exercises, this.description, this.time});
 
   // Convert to JSON
   Map<String, dynamic> toJson() {
@@ -408,6 +413,7 @@ class Program {
       title,
       description: description,
       exercises: exercises?.map((exercise) => exercise.copy()).toList(),
+      time: time,
     );
   }
 
